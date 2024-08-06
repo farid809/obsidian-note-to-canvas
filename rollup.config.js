ef031314e95c4d5eb4e1022c5367ad2748f2d5e8
farid809 <farid809@gmail.com>
2024-08-06 22:26:37 +0000

    rollup support

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'main.ts',
+  output: {
+    file: 'main.js',
+    format: 'cjs'
+  },
+  external: ['obsidian'],
+  plugins: [
+    resolve({
+      preferBuiltins: false,
+    }),
+    commonjs(),
+    typescript()
+  ]
+};
